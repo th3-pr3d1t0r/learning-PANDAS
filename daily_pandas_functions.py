cc97d524f344dfd5c2e099a73f84cb1f21cac29b
pr3d1t0r <ugochukwuanosike1@gmail.com>
2025-07-04 16:35:18 +0100

    Initial setup: Pandas imports and sample DataFrame

--- daily_pandas_functions.py
+++ daily_pandas_functions.py
@@ -1,3 +1,12 @@
-# 1. View first few rows
-print(df.head())
+import pandas as pd
+import numpy as np
 
+# Sample DataFrame
+df = pd.DataFrame({
+    'A': [1, 2, 3, 4, 5],
+    'B': [5, 6, 7, 8, 9],
+    'C': ['foo', 'bar', 'foo', 'baz', 'bar'],
+    'D': pd.date_range('2025-01-01', periods=5)
+})
+
+# ---------------------------
